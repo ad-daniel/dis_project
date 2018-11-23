44136e2b8b8a820696dc52dd439d2916f4766e4e
Mathilde <mathilde.bensimhon@epfl.ch>
2018-11-23 17:12:56 +0100

    Optimization parameters with Matlab oK
    Complement of the "Temp commit with communication working"

--- webots/Optimization parameters/matlab/Graph_performances.m
+++ webots/Optimization parameters/matlab/Graph_performances.m
@@ -58,30 +58,61 @@ while(c<=M)
 end
 
 nb_simul_real = nb_simul_real -1;
-comp_mean = [ones(nb_simul_real,1), zeros(nb_simul_real, nb_metrics)];
-comp_std = [ones(nb_simul_real,1), zeros(nb_simul_real, nb_metrics)];
-
+% comp_mean = [ones(nb_simul_real,1), zeros(nb_simul_real, nb_metrics)];
+% comp_std = [ones(nb_simul_real,1), zeros(nb_simul_real, nb_metrics)];
+comp_mean = [zeros(nb_simul_real, nb_metrics)];
+comp_std = [zeros(nb_simul_real, nb_metrics)];
 
 %Boxplot of every metrics depending on the parameters of the simulation
 for(i=2:nb_steps_per_simul:nb_metrics*nb_steps_per_simul+1)
     j = floor(i/nb_steps_per_simul)+2; %to obtain indice for data_label ref
-    comp_mean(:,j)= mean(perf_boxplot(:,i:i+nb_steps_per_simul -1) ,2);
-    comp_std(:,j) = std(perf_boxplot(:,i:i+nb_steps_per_simul -1),0,2);
+    comp_mean(:,j-1)= mean(perf_boxplot(:,i:i+nb_steps_per_simul -1) ,2);
+    comp_std(:,j-1) = std(perf_boxplot(:,i:i+nb_steps_per_simul -1),0,2);
     
-    figure; 
-    %boxplot(perf_boxplot(:,i:i+nb_steps_per_simul -1)', perf_boxplot(:,1));  %passer en 3 dimensions plus simple?
-    boxplot(perf_boxplot(:,i:i+nb_steps_per_simul -1)');
-    ylabel(data_label(j)); 
-    xlabel(data_label(2));
+%     figure; 
+%     boxplot(perf_boxplot(:,i:i+nb_steps_per_simul -1)');
+%     ylabel(data_label(j)); 
+%     xlabel('Simulation number');
 end
 
 %Comparison of performances obtained
 [best_mean_value, I_mean] = max(comp_mean,[],1);
-[best_std, I_std] = min(comp_std,[],1);
+
+comp_std_nonzero = comp_std; 
+k = find(comp_std == 0); 
+comp_std_nonzero(k) = 2; 
+
+[best_std, I_std] = min(comp_std_nonzero,[],1);
+best_perf_boxplot = zeros(nb_metrics,nb_metrics*nb_steps_per_simul, 2);
 
 for(i=1:4)
     fprintf("Best mean value for %s is simulation %d and smallest std is simulation %d\n",...
-            data_label(i+2), I_mean(i), I_std(i));
+    data_label(i+2), I_mean(i), I_std(i));
+    best_perf_boxplot(i,:,1) = perf_boxplot(I_mean(i), 2:1+nb_metrics*nb_steps_per_simul);
+    best_perf_boxplot(i,:,2) = perf_boxplot(I_std(i), 2:1+nb_metrics*nb_steps_per_simul);
 end
+%Plot the box plot related to the best performances obtained
+figure;
+title('Best mean value for');
+ns=1;
+for(j=1:2)
+    for(i=1:4)
+        subplot(2,4,ns);
+        c = nb_steps_per_simul * (i-1) + 1; 
+        %g = sprintf("Sim%d",I_mean(1), "Sim%d",I_mean(2), "Sim%d",I_mean(3), "Sim%d",I_mean(4)];
+        boxplot(best_perf_boxplot(:,c:c+nb_steps_per_simul -1,j)');
+        if(j==1)
+            xticklabels({'S.805','S.903','S.109','S.805'});
+        elseif(j==2)
+            xticklabels({'S.505','S.714','S.103','S.102'});
+        end
+        %title(['Subplot' data_label(i+2)]);
+        title({data_label(i+2)});
+        ns = ns+1; 
+    end
+%title(['Temperature is ',num2str(c),' C'])
+end
+ sgtitle('Top : Mean value maximization. Down : Std minimization');
+
 
 fclose('all');
